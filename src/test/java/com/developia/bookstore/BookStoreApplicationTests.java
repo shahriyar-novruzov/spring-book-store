244612af5f7b5e54f59e1bff6d2f0e564d5328b3
Shahriyar Novruzov <shahriyar.novruzov@pashabank.az>
2020-04-18 21:01:47 +0400

    first commit

---
+++ src/test/java/com/developia/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developia.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
